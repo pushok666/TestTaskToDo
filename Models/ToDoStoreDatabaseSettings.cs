df1078fbc83329ee388544ba17c39b1046e0c267
pushok666 <pavel.work.01@bk.ru>
2022-04-03 10:49:27 +0300

    correct

--- Models/ToDoStoreDatabaseSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace TestTaskToDo.Models;
-
-public class ToDoStoreDatabaseSettings
-{
-    public string ConnectionString {get; set;} = null!;
-    public string DatabaseName {get; set;} = null!;
-    public string ToDoCollectionName {get; set;} = null!;
-}
